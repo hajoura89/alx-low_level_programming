da012723dd4bd180588200458a7a1e95bff907b7
hajarka <hajarkammou@gmail.com>
2023-04-21 16:28:46 -0700

    Task 6 Commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-
 /**
- *main - print 0123456789 using putchar
+ *main - Entry point
  *Description: prints all single digit numbers of base 10 starting from 0
  *Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	int n = '0';
+	int n = 0;
 
-	while (n <= '9')
+	while (n <= 9)
 	{
 		putchar(n);
 		n++;
 	}
-i	putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
