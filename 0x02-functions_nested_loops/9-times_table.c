6ffb7cc3461788dad73acbd27a901c99beb7a244
hajarka <hajarkammou@gmail.com>
2023-04-25 16:50:12 -0700

    msg

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,34 +6,24 @@
 
 void times_table(void)
 {
-	int row;
-	int column;
-	int multiplication;
-
-	for (row = 0; row <= 9; row++)
+	int num, multiplied, product;
+	
+	for (num = 0; num <= 9; num++)
 	{
-		for (column = 0; column <= 9; column++)
+		_putchar (48);
+        	for (multiplied = 1; multiplied <= 9; multiplied++)
 		{
-			multiplication = (row * column);
+			_putchar (',');
+           		_putchar (' ');
+            		
+			product = num * multiplied;
+            		
+			if (product <= 9)
+                		_putchar (' ');
+			else
+				_putchar ((product / 10) + 48);
 
-			if (column == 0)
-			{
-				_putchar('0' + multiplication);
-			}
-			else if (multiplication <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + multiplication);
-			}
-			else if (multiplication > 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + (multiplication));
-				_putchar('0' + (multiplication % 10));
-			}
+			_putchar (product % 10) + 48);
 		}
 		_putchar('\n');
 	}
