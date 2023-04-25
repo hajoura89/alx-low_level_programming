edd66d505d6ab09e98f083a0ee6ffd0e02397629
hajarka <hajarkammou@gmail.com>
2023-04-25 14:40:53 -0700

    alphabet commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,8 @@
-#include<main.h>
-
+#include "main.h"
+/**
+ * print_alphabet: a function that prints the alphabet, in lowercase, followed by a new line.
+ *
+*/
 void print_alphabet(void)
 {
 	char alphabet;
