2db548ed1da0beee9c2455dab7ea6949a3ea8eee
hajarka <hajarkammou@gmail.com>
2023-04-25 17:02:01 -0700

    msg

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integers and returns the result.
+ * @a: first integer argument
+ * @b: second integer argument
+ * Return: Addition of a & b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,26 +1,25 @@
 #include "main.h"
 
 /**
- * times_table - a function that prints the 9 times table, starting with 0.
+* times_table - a function that prints the 9 times table, starting with 0.
 */
 
 void times_table(void)
 {
-	int num, multiplied, product;
+	int num, multiplied, product;	
 	
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar (48);
-        	for (multiplied = 1; multiplied <= 9; multiplied++)
+		for (multiplied = 1; multiplied <= 9; multiplied++)
 		{
 			_putchar (',');
-           		_putchar (' ');
-            		
+			_putchar (' ');	
+			
 			product = num * multiplied;
-            		
 			if (product <= 9)
-                		_putchar (' ');
-			else
+				_putchar (' ');
+			els
 				_putchar ((product / 10) + 48);
 
 			_putchar ((product % 10) + 48);
