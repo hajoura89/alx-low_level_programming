d917c606192be0c4851d46706b22356ca169c397
hajarka <hajarkammou@gmail.com>
2023-05-02 16:35:18 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s;
+	char c;
+
+	srand(time(NULL));
+	while (s <= 2645)
+	{
+		c = rand() % 128;
+		s += c;
+		_putchar(c);
+	}
+	_putchar(2772 - s);
+	return (0);
+}
