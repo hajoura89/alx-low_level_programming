7c9c6df38c674d042412953afa66f0525e3c7b44
hajarka <hajarkammou@gmail.com>
2023-05-18 17:23:38 -0700

    0x0C. C - More malloc, free Commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -55,32 +55,32 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	int lens1, lens2, len, i, carry, dg1, dg2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
+	lens1 = _strlen(s1);
+	lens2 = _strlen(s2);
+	len = lens1 + lens2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (i = 0; i <= len1 + len2; i++)
+	for (i = 0; i <= lens1 + lens2; i++)
 		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	for (lens1 = lens1 - 1; lens1 >= 0; lens1--)
 	{
-		digit1 = s1[len1] - '0';
+		dg1 = s1[lens1] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		for (lens2 = _strlen(s2) - 1; lens2 >= 0; lens2--)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
+			dg2 = s2[lens2] - '0';
+			carry += result[lens1 + lens2 + 1] + (dg1 * dg2);
+			result[lens1 + lens2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+			result[lens1 + lens2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
 	{
