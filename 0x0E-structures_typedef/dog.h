4157b14f55b94d0c9a1dc5a80a6bf3456376a3e9
hajarka <hajarkammou@gmail.com>
2023-05-22 06:03:40 -0700

    0x0E. C - Structures, typedef commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ */
+struct dog
+{
+   char *name;
+   float age;
+   char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef  struct dog dog_t;
+
+#endif /* DOG_H */
