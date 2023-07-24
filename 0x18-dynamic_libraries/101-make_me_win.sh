532be8b9ef491cd2ef3c83b0906af3802c035cff
hajoura89 <hajarkammou@gmail.com>
2023-07-24 10:15:11 -0700

    Task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+#wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+#export LD_PRELOAD=/tmp/nrandom.so
+
+#!/bin/bash
+export LD_PRELOAD=/$PWD/libtest.so
